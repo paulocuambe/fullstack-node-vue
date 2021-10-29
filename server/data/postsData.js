ed224f5f3d6e3d45ae0fc731c462fc93675fb5d5
paulocuambe <paulo.cuambe@sciafrica.net>
2021-10-29 17:53:45 +0200

    order by date desc by default

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -2,7 +2,7 @@ const database = require("../infra/database.js");
 
 module.exports = {
   getPosts: function () {
-    return database.query("select * from blog.post");
+    return database.query("select * from blog.post order by date desc");
   },
 
   getPost: function (id) {
